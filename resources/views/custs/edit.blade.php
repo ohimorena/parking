fedc9962f4b2ef7c42eb169bafe2753fedcddbc2
obukhova.e.v <obukhova.e.v@mail.ru>
2023-12-11 09:43:06 +0300

    added styles for buttons

--- resources/views/custs/edit.blade.php
+++ resources/views/custs/edit.blade.php
@@ -10,172 +10,175 @@
 
   @if ($errors->any())
     <div class="alert alert-danger">
-        <ul>
-            @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-        </ul>
+      <ul>
+        @foreach ($errors->all() as $error)
+          <li>{{ $error }}</li>
+        @endforeach
+      </ul>
     </div>
   @endif
 
-  <form action="{{ route('custs.update', $cust->id) }}" method="post">
-    @csrf
-    @method('patch')
+  <div class="form-area">
 
-    <div class="row mb-3">
-      <label for="inputEmail3" class="col-sm-2 col-form-label">Фамилия, имя, отчество</label>
-      <div class="col-sm-10">
-        <input type="text" name="full_name" value="{{ $cust->full_name }}" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
+    <form action="{{ route('custs.update', $cust->id) }}" method="post">
+      @csrf
+      @method('patch')
+      <div class="row mb-3">
+        <label for="inputEmail3" class="col-sm-2 col-form-label">Фамилия, имя, отчество</label>
+        <div class="col-sm-10">
+          <input type="text" name="full_name" value="{{ $cust->full_name }}" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
+        </div>
       </div>
-    </div>
 
-    <div>
-      <fieldset>
-        <div class="row mb-3">
-          <legend class="col-form-label col-sm-2 pt-0">Пол</legend>
-          <div class="col-sm-10">
-            <div class="form-check">
-              <input class="form-check-input" type="radio" name="sex" id="sex1" value="мужской" {{ ($cust->sex == "мужской")? "checked" : "" }}>
-              <label class="form-check-label" for="sex1">
-                мужской
-              </label>
-            </div>
-            <div class="form-check">
-              <input class="form-check-input" type="radio" name="sex" id="sex2" value="женский" {{ ($cust->sex == "женский")? "checked" : "" }}>
-              <label class="form-check-label" for="sex2">
-                женский
-              </label>
+      <div>
+        <fieldset>
+          <div class="row mb-3">
+            <legend class="col-form-label col-sm-2 pt-0">Пол</legend>
+            <div class="col-sm-10">
+              <div class="form-check">
+                <input class="form-check-input" type="radio" name="sex" id="sex1" value="мужской" {{ ($cust->sex == "мужской")? "checked" : "" }}>
+                <label class="form-check-label" for="sex1">
+                  мужской
+                </label>
+              </div>
+              <div class="form-check">
+                <input class="form-check-input" type="radio" name="sex" id="sex2" value="женский" {{ ($cust->sex == "женский")? "checked" : "" }}>
+                <label class="form-check-label" for="sex2">
+                  женский
+                </label>
+              </div>
             </div>
           </div>
+        </fieldset>
+      </div>
+
+      <div class="row mb-3">
+        <label for="inputEmail3" class="col-sm-2 col-form-label">Телефон</label>
+        <div class="col-sm-10">
+          <input type="text" name="phone" value="{{ $cust->phone }}" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
         </div>
-      </fieldset>
-    </div>
-
-    <div class="row mb-3">
-      <label for="inputEmail3" class="col-sm-2 col-form-label">Телефон</label>
-      <div class="col-sm-10">
-        <input type="text" name="phone" value="{{ $cust->phone }}" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
       </div>
-    </div>
 
-    <div class="row mb-3">
-      <label for="inputEmail3" class="col-sm-2 col-form-label">Адрес</label>
-      <div class="col-sm-10">
-        <input type="text" name="address" value="{{ $cust->address }}" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
+      <div class="row mb-3">
+        <label for="inputEmail3" class="col-sm-2 col-form-label">Адрес</label>
+        <div class="col-sm-10">
+          <input type="text" name="address" value="{{ $cust->address }}" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
+        </div>
       </div>
-    </div>
-    
-    <div align=center>
-      <input type="submit" value="Сохранить" class="btn btn-info btn-md">
-    </div><br>
+      
+      <div class="button-save-1">
+        <input type="submit" value="Сохранить" class="btn btn-primary">
+      </div>
+    </form>
 
-  </form>
+    <form action="{{ route('custs.destroy', $cust->id) }}" method="post">
+      @csrf
+      @method('delete')
+      <div class="button-delete">
+        <input type="submit" value="Удалить" class="btn btn-danger">
+      </div>
+    </form>
 
-  <form action="{{ route('custs.destroy', $cust->id) }}" method="post">
-    @csrf
-    @method('delete')
-    <div align=center>
-      <input type="submit" value="Удалить клиента" class="btn btn-danger btn-md">
-    </div><br>
-  </form>
+  </div>
 
   <div>
     <p><h4>Автомобили</h4></p>
   </div>
 
   @foreach($autos as $auto)
-  <form action="{{ route('autos.update', $auto->id) }}" method="post">
-    @csrf
-    @method('patch')  
+    <div class="form-area">
+      <form action="{{ route('autos.update', $auto->id) }}" method="post">
+        @csrf
+        @method('patch')  
+          <div class="row mb-3">
+            <label for="inputEmail3" class="col-sm-2 col-form-label">Марка</label>
+            <div class="col-sm-10">
+              <input type="text" name="brand" value="{{ $auto->brand }}" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
+            </div>
+          </div>
+
+          <div class="row mb-3">
+            <label for="inputEmail3" class="col-sm-2 col-form-label">Модель</label>
+            <div class="col-sm-10">
+              <input type="text" name="model" value="{{ $auto->model }}" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
+            </div>
+          </div>
+
+          <div class="row mb-3">
+            <label for="inputEmail3" class="col-sm-2 col-form-label">Цвет кузова</label>
+            <div class="col-sm-10">
+              <input type="text" name="color" value="{{ $auto->color }}" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
+            </div>
+          </div>
+
+          <div class="row mb-3">
+            <label for="inputEmail3" class="col-sm-2 col-form-label">Государственный номер РФ</label>
+            <div class="col-sm-10">
+              <input type="text" name="reg_number" value="{{ $auto->reg_number }}" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
+            </div>
+          </div>
+
+          <div class="form-check">
+            <input class="form-check-input" type="checkbox" name="is_parking" value="1" {{ ($auto->is_parking == "1")? "checked" : "" }} id="flexCheckDefault">
+            <label class="form-check-label" for="flexCheckDefault">
+              Автомобиль находится на стоянке в данный момент
+            </label>
+          </div>
+
+          <div class="button-save-2">
+            <input type="submit" value="Сохранить" class="btn btn-primary">
+          </div>
+      </form>
+    </div>
+  @endforeach
+
+  <div class="form-area">
+    <form action="{{ route('autos.store') }}" method="post">
+      @csrf
+      <input type="hidden" name="cust_id" value="{{ $cust->id }}">
+
       <div class="row mb-3">
         <label for="inputEmail3" class="col-sm-2 col-form-label">Марка</label>
         <div class="col-sm-10">
-          <input type="text" name="brand" value="{{ $auto->brand }}" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
+          <input type="text" name="brand" value="{{ old('brand') }}" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
         </div>
       </div>
 
       <div class="row mb-3">
         <label for="inputEmail3" class="col-sm-2 col-form-label">Модель</label>
         <div class="col-sm-10">
-          <input type="text" name="model" value="{{ $auto->model }}" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
+          <input type="text" name="model" value="{{ old('model') }}" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
         </div>
       </div>
 
       <div class="row mb-3">
         <label for="inputEmail3" class="col-sm-2 col-form-label">Цвет кузова</label>
         <div class="col-sm-10">
-          <input type="text" name="color" value="{{ $auto->color }}" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
+          <input type="text" name="color" value="{{ old('color') }}" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
         </div>
       </div>
 
       <div class="row mb-3">
         <label for="inputEmail3" class="col-sm-2 col-form-label">Государственный номер РФ</label>
         <div class="col-sm-10">
-          <input type="text" name="reg_number" value="{{ $auto->reg_number }}" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
+          <input type="text" name="reg_number" value="{{ old('reg_number') }}" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
         </div>
       </div>
 
       <div class="form-check">
-        <input class="form-check-input" type="checkbox" name="is_parking" value="1" {{ ($auto->is_parking == "1")? "checked" : "" }} id="flexCheckDefault">
+        <input type="hidden" name="is_parking" value="0">
+        <input class="form-check-input" type="checkbox" name="is_parking" value="1" id="flexCheckDefault">
         <label class="form-check-label" for="flexCheckDefault">
           Автомобиль находится на стоянке в данный момент
         </label>
       </div>
 
-      <div align=center>
-        <input type="submit" value="Сохранить" class="btn btn-info btn-md">
-      </div><br>
-  </form>
-  @endforeach
-
-  <form action="{{ route('autos.store') }}" method="post">
-  @csrf
-  <input type="hidden" name="cust_id" value="{{ $cust->id }}">
-
-  <div class="row mb-3">
-      <label for="inputEmail3" class="col-sm-2 col-form-label">Марка</label>
-      <div class="col-sm-10">
-        <input type="text" name="brand" value="{{ old('brand') }}" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
+      <div class="button-save-2">
+        <input type="submit" value="Сохранить" class="btn btn-primary">
       </div>
-    </div>
-
-    <div class="row mb-3">
-      <label for="inputEmail3" class="col-sm-2 col-form-label">Модель</label>
-      <div class="col-sm-10">
-        <input type="text" name="model" value="{{ old('model') }}" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
-      </div>
-    </div>
-
-    <div class="row mb-3">
-      <label for="inputEmail3" class="col-sm-2 col-form-label">Цвет кузова</label>
-      <div class="col-sm-10">
-        <input type="text" name="color" value="{{ old('color') }}" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
-      </div>
-    </div>
-
-    <div class="row mb-3">
-      <label for="inputEmail3" class="col-sm-2 col-form-label">Государственный номер РФ</label>
-      <div class="col-sm-10">
-        <input type="text" name="reg_number" value="{{ old('reg_number') }}" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
-      </div>
-    </div>
-
-    <div class="form-check">
-      <input class="form-check-input" type="checkbox" name="is_parking" value="1" id="flexCheckDefault">
-      <label class="form-check-label" for="flexCheckDefault">
-        Автомобиль находится на стоянке в данный момент
-      </label>
-    </div>
-
-    <div align=center>
-      <input type="submit" value="Сохранить" class="btn btn-info btn-md">
-    </div><br>
-  </form>
-      
+    </form>
   </div>
 </div>
 
-
-</div>
 @endsection 
     
